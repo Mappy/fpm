b5daf9258584aa994bdaee48469814e14e62b956
Ignishky <thierry.ducloyer@gmail.com>
2017-09-21 16:55:50 +0200

    [Tomtom Download] Improve code coverage.

--- src/main/java/com/mappy/fpm/batches/tomtom/download/TomtomDownloader.java
+++ src/main/java/com/mappy/fpm/batches/tomtom/download/TomtomDownloader.java
@@ -4,27 +4,41 @@ import com.mappy.fpm.batches.tomtom.download.MetalinkParser.Metalink;
 import com.mappy.fpm.batches.tomtom.download.MetalinkParser.MetalinkUrl;
 import com.mappy.fpm.batches.tomtom.download.TomtomCountries.TomtomCountry;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.Sets.union;
 import static com.mappy.fpm.batches.tomtom.download.TomtomCountries.countries;
 import static com.mappy.fpm.batches.tomtom.download.TomtomCountries.outerworld;
+import static org.apache.http.impl.NoConnectionReuseStrategy.INSTANCE;
 
 @Slf4j
 public class TomtomDownloader {
 
     private final MetalinkDownloader downloader;
     private final ShapefileDownloader shpDownloader;
-    private final List<TomtomCountry> countries;
+    private final Set<TomtomCountry> countries;
 
-    public TomtomDownloader(MetalinkDownloader downloader, ShapefileDownloader shpDownloader, List<TomtomCountry> countries) {
+    public TomtomDownloader(MetalinkDownloader downloader, ShapefileDownloader shpDownloader, Set<TomtomCountry> countries) {
         this.downloader = downloader;
         this.shpDownloader = shpDownloader;
-        this.countries = countries;
+        this.countries = countries != null ? countries : union(countries(), outerworld());
+    }
+
+    public static void main(String args[]) throws IOException {
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Should have 4 arguments : tomtomFolder, tomtomVersion, tomtomLogin, tomtomPassword");
+        }
+        File outputDirectory = new File(args[0]);
+        outputDirectory.mkdirs();
+        HttpClient httpClient = HttpClientBuilder.create().setMaxConnPerRoute(10).setConnectionReuseStrategy(INSTANCE).build();
+        new TomtomDownloader(new MetalinkDownloader(args[2], args[3], args[1], HttpClientBuilder.create().build()), new ShapefileDownloader(outputDirectory, httpClient), null).run();
     }
 
     public void run() throws IOException {
@@ -52,18 +66,5 @@ public class TomtomDownloader {
                 }
             }
         });
-    }
-
-    public static void main(String args[]) throws IOException {
-        if (args.length != 4) {
-            throw new IllegalArgumentException("Should have 4 arguments : tomtomFolder, tomtomVersion, tomtomLogin, tomtomPassword");
-        }
-        File outputDirectory = new File(args[0]);
-        outputDirectory.mkdirs();
-
-        List<TomtomCountry> countries = countries();
-        countries.addAll(outerworld());
-
-        new TomtomDownloader(new MetalinkDownloader(args[2], args[3], args[1], HttpClientBuilder.create().build()), new ShapefileDownloader(outputDirectory), countries).run();
     }
 }
---
+++ src/test/java/com/mappy/fpm/batches/tomtom/download/ShapefileDownloaderTest.java
@@ -0,0 +1,54 @@
+package com.mappy.fpm.batches.tomtom.download;
+
+import com.mappy.fpm.batches.tomtom.download.MetalinkParser.MetalinkUrl;
+import com.mappy.fpm.batches.tomtom.download.TomtomCountries.TomtomCountry;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.BasicHttpEntity;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ShapefileDownloaderTest {
+
+    private ShapefileDownloader shapefileDownloader;
+
+    @Before
+    public void setUp() throws Exception {
+        BasicHttpEntity httpEntity = new BasicHttpEntity();
+        httpEntity.setContent(getClass().getResourceAsStream("/tomtom/download/and.7z.001"));
+
+        HttpResponse httpResponse = mock(HttpResponse.class);
+        when(httpResponse.getEntity()).thenReturn(httpEntity);
+
+        HttpClient httpClient = mock(HttpClient.class);
+        when(httpClient.execute(any(HttpGet.class))).thenReturn(httpResponse);
+
+        shapefileDownloader = new ShapefileDownloader(new File("target"), httpClient);
+    }
+
+    @Test
+    public void should_download_shapefiles() {
+        File folder = new File("target/Andorre");
+        if (folder.exists()) {
+            Stream.of(folder.listFiles()).forEach(file -> file.delete());
+            folder.delete();
+        }
+        assertThat(folder.exists()).isFalse();
+
+        TomtomCountry country = new TomtomCountry("AND", "Andorre");
+        MetalinkUrl metalinkUrl = MetalinkUrl.parse("eur2016_09-shpd-sp-and-ax.7z.001", "http://url1");
+
+        shapefileDownloader.download(country, metalinkUrl);
+
+        assertThat(folder.listFiles()).hasSize(2);
+    }
+}
