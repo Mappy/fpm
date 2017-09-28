1dd54665753f02757b96bd92226dd6858fe4c05e
Ignishky <thierry.ducloyer@gmail.com>
2017-09-28 16:20:13 +0200

    [Refacto] test A8 with unit test.

--- src/test/java/com/mappy/fpm/batches/tomtom/shapefiles/BoundariesA8ShapefileTest.java
+++ src/test/java/com/mappy/fpm/batches/tomtom/shapefiles/BoundariesA8ShapefileTest.java
@@ -5,8 +5,12 @@ import com.mappy.fpm.batches.tomtom.TomtomFolder;
 import com.mappy.fpm.batches.tomtom.dbf.names.NameProvider;
 import com.mappy.fpm.batches.tomtom.helpers.OsmLevelGenerator;
 import com.mappy.fpm.batches.tomtom.helpers.TownTagger;
+import com.mappy.fpm.batches.tomtom.helpers.TownTagger.Centroid;
 import com.mappy.fpm.batches.utils.GeometrySerializer;
 import com.mappy.fpm.batches.utils.OsmosisSerializer;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.impl.PackedCoordinateSequence;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -21,10 +25,14 @@ import static java.nio.file.Files.createDirectory;
 import static java.nio.file.Paths.get;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class BoundariesA8ShapefileTest {
-    public static Tomtom2OsmTestUtils.PbfContent pbfContent;
+
+    private static Tomtom2OsmTestUtils.PbfContent pbfContent;
+
+    private static NameProvider nameProvider = mock(NameProvider.class);
 
     @BeforeClass
     public static void setup() throws Exception {
@@ -33,18 +41,22 @@ public class BoundariesA8ShapefileTest {
             createDirectory(dir);
         }
 
-        NameProvider nameProvider = mock(NameProvider.class);
         Map<String, String> names = newHashMap();
         names.putAll(of("name", "Brussel", "name:nl", "Bruxelles", "name:fr", "Bruxelles"));
         when(nameProvider.getAlternateNames(10560000000250L)).thenReturn(names);
 
         Map<String, String> names2 = newHashMap();
-        names.putAll(of("name", "Brussel", "name:nl", "Bruxelles", "name:fr", "Bruxelles"));
+        names2.putAll(of("name", "Brussel", "name:nl", "Bruxelles", "name:fr", "Bruxelles"));
         when(nameProvider.getAlternateNames(10560000000267L)).thenReturn(names2);
 
         Map<String, String> names3 = newHashMap();
-        names.putAll(of("name", "Brussel", "name:nl", "Bruxelles", "name:fr", "Bruxelles"));
+        names3.putAll(of("name", "Brussel", "name:nl", "Bruxelles", "name:fr", "Bruxelles"));
         when(nameProvider.getAlternateNames(10560000000263L)).thenReturn(names3);
+
+        Map<String, String> names4 = newHashMap();
+        names4.putAll(of("name", "Brussel", "name:nl", "Bruxelles", "name:fr", "Bruxelles"));
+        when(nameProvider.getAlternateNames(10560000718742L)).thenReturn(names4);
+
 
         TomtomFolder tomtomFolder = mock(TomtomFolder.class);
         when(tomtomFolder.getFile("___a8.shp")).thenReturn("src/test/resources/tomtom/boundaries/a8/belbe3___________a8.shp");
@@ -53,6 +65,10 @@ public class BoundariesA8ShapefileTest {
         when(osmLevelGenerator.getOsmLevel("belbe3", "8")).thenReturn("8");
 
         TownTagger townTagger = mock(TownTagger.class);
+        double[] doubles3 = {4.307077, 50.8366041};
+        GeometryFactory factory = mock(GeometryFactory.class);
+        Point point = new Point(new PackedCoordinateSequence.Double(doubles3, 2), factory);
+        when(townTagger.get(10560000718742L)).thenReturn(new Centroid(10560000718742L, "toto", 8, 1, 7, point));
 
         BoundariesA8Shapefile shapefile = new BoundariesA8Shapefile(tomtomFolder, nameProvider, osmLevelGenerator, townTagger);
 
@@ -86,6 +102,7 @@ public class BoundariesA8ShapefileTest {
 
     @Test
     public void should_have_relation_with_admin_centers_and_all_tags() throws Exception {
+        verify(nameProvider).getAlternateNames(10560000718742L);
 
         assertThat(pbfContent.getRelations().stream().flatMap(relation -> relation.getMembers().stream())) //
                 .filteredOn(relationMember -> relationMember.getRole().equals("admin_center")) //
@@ -109,15 +126,7 @@ public class BoundariesA8ShapefileTest {
 
         assertThat(pbfContent.getNodes().stream())
                 .filteredOn(node -> node.getTags().hasKeyValue("name", "Brussel")) //
-                .filteredOn(node -> node.getTags().hasKeyValue("capital", "yes")) //
+                .filteredOn(node -> node.getTags().hasKeyValue("capital", "8")) //
                 .filteredOn(node -> node.getTags().hasKeyValue("place", "city")).hasSize(1);
     }
-
-    @Test
-    public void should_have_some_brussel_neighbourhood() throws Exception {
-
-        assertThat(pbfContent.getNodes().stream())
-                .filteredOn(node -> node.getTags().hasKeyValue("place", "neighbourhood")).isNotEmpty();
-    }
-
 }
