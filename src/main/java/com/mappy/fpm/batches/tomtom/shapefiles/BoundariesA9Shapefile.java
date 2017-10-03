c4ec7bfbd485b9ecc3b9a6f12d5fe9fdf8d7c61f
Ignishky <thierry.ducloyer@gmail.com>
2017-10-03 15:24:59 +0200

    [Refacto] Add unit tests for A9.

--- src/main/java/com/mappy/fpm/batches/tomtom/shapefiles/BoundariesA9Shapefile.java
+++ src/main/java/com/mappy/fpm/batches/tomtom/shapefiles/BoundariesA9Shapefile.java
@@ -4,13 +4,52 @@ import com.mappy.fpm.batches.tomtom.TomtomFolder;
 import com.mappy.fpm.batches.tomtom.dbf.names.NameProvider;
 import com.mappy.fpm.batches.tomtom.helpers.BoundariesShapefile;
 import com.mappy.fpm.batches.tomtom.helpers.OsmLevelGenerator;
+import com.mappy.fpm.batches.tomtom.helpers.TownTagger;
+import com.mappy.fpm.batches.utils.Feature;
+import com.mappy.fpm.batches.utils.GeometrySerializer;
+import org.openstreetmap.osmosis.core.domain.v0_6.EntityType;
+import org.openstreetmap.osmosis.core.domain.v0_6.Node;
+import org.openstreetmap.osmosis.core.domain.v0_6.RelationMember;
 
 import javax.inject.Inject;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static com.google.common.collect.Maps.newHashMap;
+import static java.util.Optional.ofNullable;
 
 public class BoundariesA9Shapefile extends BoundariesShapefile {
 
+    private final TownTagger townTagger;
+
     @Inject
-    public BoundariesA9Shapefile(TomtomFolder folder, NameProvider nameProvider, OsmLevelGenerator osmLevelGenerator) {
+    public BoundariesA9Shapefile(TomtomFolder folder, NameProvider nameProvider, OsmLevelGenerator osmLevelGenerator, TownTagger townTagger) {
         super(folder.getFile("___a9.shp"), 9, nameProvider, osmLevelGenerator);
+        this.townTagger = townTagger;
+    }
+
+    @Override
+    public void finishRelation(GeometrySerializer serializer, Map<String, String> adminTags, List<RelationMember> members, Feature feature) {
+
+        TownTagger.Centroid cityCenter = townTagger.get(feature.getLong("CITYCENTER"));
+
+        if (cityCenter != null) {
+            Map<String, String> tags = newHashMap();
+
+            tags.put("name", cityCenter.getName());
+            tags.putAll(nameProvider.getAlternateCityNames(cityCenter.getId()));
+
+            ofNullable(feature.getLong("POP")).ifPresent(pop -> tags.put("population", String.valueOf(pop)));
+
+            if(serializer.containPoint(cityCenter.getPoint())) {
+                cityCenter.getPoint().getCoordinate().x = cityCenter.getPoint().getCoordinate().x +0.000001;
+                cityCenter.getPoint().getCoordinate().y = cityCenter.getPoint().getCoordinate().y +0.000001;
+            }
+            Optional<Node> node = serializer.writePoint(cityCenter.getPoint(), tags);
+            node.ifPresent(adminCenter -> members.add(new RelationMember(adminCenter.getId(), EntityType.Node, "admin_center")));
+        }
+
+        serializer.writeRelation(members, adminTags);
     }
 }
--- src/test/java/com/mappy/fpm/batches/tomtom/shapefiles/BoundariesA9ShapefileTest.java
+++ src/test/java/com/mappy/fpm/batches/tomtom/shapefiles/BoundariesA9ShapefileTest.java
@@ -6,7 +6,6 @@ import com.mappy.fpm.batches.tomtom.TomtomFolder;
 import com.mappy.fpm.batches.tomtom.dbf.names.NameProvider;
 import com.mappy.fpm.batches.tomtom.helpers.OsmLevelGenerator;
 import com.mappy.fpm.batches.tomtom.helpers.TownTagger;
-import com.mappy.fpm.batches.tomtom.helpers.TownTagger.Centroid;
 import com.mappy.fpm.batches.utils.GeometrySerializer;
 import com.mappy.fpm.batches.utils.OsmosisSerializer;
 import com.vividsolutions.jts.geom.GeometryFactory;
@@ -27,7 +26,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class BoundariesA8ShapefileTest extends AbstractTest {
+public class BoundariesA9ShapefileTest extends AbstractTest {
 
     private static PbfContent pbfContent;
 
@@ -36,11 +35,11 @@ public class BoundariesA8ShapefileTest extends AbstractTest {
     @BeforeClass
     public static void setup() throws Exception {
 
-        when(nameProvider.getAlternateNames(10560000000250L)) //
+        when(nameProvider.getAlternateNames(10560000000247L)) //
                 .thenReturn(of("name", "Anderlecht", "name:nl", "AnderlechtNL", "name:fr", "AnderlechtFR"));
-        when(nameProvider.getAlternateNames(10560000000267L)) //
+        when(nameProvider.getAlternateNames(10560000000262L)) //
                 .thenReturn(of("name", "Sint-Gillis", "name:nl", "Sint-GillisNL", "name:fr", "Sint-GillisFR"));
-        when(nameProvider.getAlternateNames(10560000000263L)) //
+        when(nameProvider.getAlternateNames(10560000000258L)) //
                 .thenReturn(of("name", "Vorst", "name:nl", "VorstNL", "name:fr", "VorstFR"));
 
         when(nameProvider.getAlternateCityNames(10560000718742L)) //
@@ -51,50 +50,48 @@ public class BoundariesA8ShapefileTest extends AbstractTest {
                 .thenReturn(of("name", "Vorst", "name:nl", "VorstCNL", "name:fr", "VorstCFR"));
 
         TomtomFolder tomtomFolder = mock(TomtomFolder.class);
-        when(tomtomFolder.getFile("a8.shp")).thenReturn("src/test/resources/tomtom/boundaries/a8/Anderlecht___________a8.shp");
+        when(tomtomFolder.getFile("___a9.shp")).thenReturn("src/test/resources/tomtom/boundaries/a9/Anderlecht___________a9.shp");
 
         OsmLevelGenerator osmLevelGenerator = mock(OsmLevelGenerator.class);
-        when(osmLevelGenerator.getOsmLevel("Anderlecht", "8")).thenReturn("8");
+        when(osmLevelGenerator.getOsmLevel("Anderlecht", "9")).thenReturn("9");
 
         TownTagger townTagger = mock(TownTagger.class);
         double[] doubles = {4.3451859, 50.8251293};
         GeometryFactory factory = mock(GeometryFactory.class);
         Point point = new Point(new PackedCoordinateSequence.Double(doubles, 2), factory);
-        when(townTagger.get(10560000718742L)).thenReturn(new Centroid(10560000718742L, "Anderlecht", 8, 1, 7, point));
+        when(townTagger.get(10560000718742L)).thenReturn(new TownTagger.Centroid(10560000718742L, "Anderlecht", 8, 1, 7, point));
         double[] doubles2 = {4.3134424, 50.8055758};
         Point point2 = new Point(new PackedCoordinateSequence.Double(doubles2, 2), factory);
-        when(townTagger.get(10560000388234L)).thenReturn(new Centroid(10560000388234L, "Sint-Gillis", 8, 1, 8, point2));
+        when(townTagger.get(10560000388234L)).thenReturn(new TownTagger.Centroid(10560000388234L, "Sint-Gillis", 8, 1, 8, point2));
         double[] doubles3 = {4.307077, 50.8366041};
         Point point3 = new Point(new PackedCoordinateSequence.Double(doubles3, 2), factory);
-        when(townTagger.get(10560000455427L)).thenReturn(new Centroid(10560000455427L, "Vorst", 8, 1, 8, point3));
+        when(townTagger.get(10560000455427L)).thenReturn(new TownTagger.Centroid(10560000455427L, "Vorst", 8, 1, 8, point3));
 
-        BoundariesA8Shapefile shapefile = new BoundariesA8Shapefile(tomtomFolder, nameProvider, osmLevelGenerator, townTagger);
-
-        GeometrySerializer serializer = new OsmosisSerializer("target/tests/Anderlecht.osm.pbf", "Test_TU");
-
+        BoundariesA9Shapefile shapefile = new BoundariesA9Shapefile(tomtomFolder, nameProvider, osmLevelGenerator, townTagger);
+        GeometrySerializer serializer = new OsmosisSerializer("target/tests/AnderlechtA9.osm.pbf", "Test_TU");
         shapefile.serialize(serializer);
         serializer.close();
 
-        pbfContent = read(new File("target/tests/Anderlecht.osm.pbf"));
+        pbfContent = read(new File("target/tests/AnderlechtA9.osm.pbf"));
         assertThat(pbfContent.getRelations()).hasSize(3);
     }
 
     @Test
-    public void should_have_relations_with_all_tags() throws Exception {
-
+    public void should_have_relations_with_all_tags() {
         List<Tags> tags = pbfContent.getRelations().stream()
                 .map(Entity::getTags)
                 .collect(toList());
 
+        assertThat(tags).hasSize(3);
         assertThat(tags).extracting(t -> t.get("boundary")).containsOnly("administrative");
-        assertThat(tags).extracting(t -> t.get("admin_level")).containsOnly("8");
+        assertThat(tags).extracting(t -> t.get("admin_level")).containsOnly("9");
         assertThat(tags).extracting(t -> t.get("type")).containsOnly("boundary");
         assertThat(tags).extracting(t -> t.get("name")).containsOnly("Anderlecht", "Sint-Gillis", "Vorst");
         assertThat(tags).extracting(t -> t.get("name:fr")).containsOnly("AnderlechtFR", "Sint-GillisFR", "VorstFR");
         assertThat(tags).extracting(t -> t.get("name:nl")).containsOnly("AnderlechtNL", "Sint-GillisNL", "VorstNL");
-        assertThat(tags).extracting(t -> t.get("population")).containsOnly("116332", "50472", "55012");
-        assertThat(tags).extracting(t -> t.get("ref:INSEE")).containsOnly("21001", "21013", "21007");
-        assertThat(tags).extracting(t -> t.get("ref:tomtom")).containsOnly("10560000000250", "10560000000267", "10560000000263");
+        assertThat(tags).extracting(t -> t.get("population")).containsOnly("0", "0", "0");
+        assertThat(tags).extracting(t -> t.get("ref:INSEE")).containsOnly("21001A", "21013A", "21007A");
+        assertThat(tags).extracting(t -> t.get("ref:tomtom")).containsOnly("10560000000247", "10560000000262", "10560000000258");
     }
 
     @Test
@@ -109,7 +106,7 @@ public class BoundariesA8ShapefileTest extends AbstractTest {
         assertThat(tags).hasSize(17);
         assertThat(tags).extracting(t -> t.get("name")).containsOnly("Anderlecht", "Sint-Gillis", "Vorst");
         assertThat(tags).extracting(t -> t.get("boundary")).containsOnly("administrative");
-        assertThat(tags).extracting(t -> t.get("admin_level")).containsOnly("8");
+        assertThat(tags).extracting(t -> t.get("admin_level")).containsOnly("9");
     }
 
     @Test
@@ -122,25 +119,29 @@ public class BoundariesA8ShapefileTest extends AbstractTest {
                 .collect(toList());
 
         assertThat(tags).hasSize(3);
-        assertThat(tags).extracting(t -> t.get("ref:tomtom")).containsOnly("10560000000250", "10560000000267", "10560000000263");
+        assertThat(tags.get(0)).hasSize(6);
+        assertThat(tags).extracting(t -> t.get("ref:tomtom")).containsOnly("10560000000247", "10560000000262", "10560000000258");
         assertThat(tags).extracting(t -> t.get("name")).containsOnly("Anderlecht", "Sint-Gillis", "Vorst");
         assertThat(tags).extracting(t -> t.get("name:fr")).containsOnly("AnderlechtFR", "Sint-GillisFR", "VorstFR");
-        assertThat(tags).extracting(t -> t.get("ref:INSEE")).containsOnly("21001", "21013", "21007");
+        assertThat(tags).extracting(t -> t.get("ref:INSEE")).containsOnly("21001A", "21013A", "21007A");
         assertThat(tags).extracting(t -> t.get("name:nl")).containsOnly("AnderlechtNL", "Sint-GillisNL", "VorstNL");
-        assertThat(tags).extracting(t -> t.get("population")).containsOnly("116332", "50472", "55012");
+        assertThat(tags).extracting(t -> t.get("population")).containsOnly("0", "0", "0");
     }
 
     @Test
-    public void should_have_Anderlecht_as_capital() throws Exception {
-        List<Tags> tags = pbfContent.getNodes().stream()
-                .filter(node -> node.getTags().size() != 0)
-                .map(Entity::getTags)
+    public void should_have_relation_with_tags_and_admin_centers() throws Exception {
+
+        List<Tags> tags = pbfContent.getRelations().stream()
+                .flatMap(f -> f.getMembers().stream())
+                .filter(relationMember -> "admin_center".equals(relationMember.getRole()))
+                .map(m -> m.getEntity().getTags())
                 .collect(toList());
 
-        assertThat(tags).hasSize(6);
+        assertThat(tags).hasSize(3);
         assertThat(tags).extracting(t -> t.get("name")).containsOnly("Anderlecht", "Sint-Gillis", "Vorst");
-        assertThat(tags).extracting(t -> t.get("name:fr")).containsOnly("AnderlechtCFR", "AnderlechtFR", "Sint-GillisCFR", "Sint-GillisFR", "VorstCFR", "VorstFR");
-        assertThat(tags).extracting(t -> t.get("name:nl")).containsOnly("AnderlechtCNL", "AnderlechtNL", "Sint-GillisCNL", "Sint-GillisNL", "VorstCNL", "VorstNL");
-        assertThat(tags).extracting(t -> t.get("population")).containsOnly("116332", "50472", "55012");
+        assertThat(tags).extracting(t -> t.get("name:fr")).containsOnly("AnderlechtCFR", "Sint-GillisCFR", "VorstCFR");
+        assertThat(tags).extracting(t -> t.get("name:nl")).containsOnly("AnderlechtCNL", "Sint-GillisCNL", "VorstCNL");
+        assertThat(tags).extracting(t -> t.get("population")).containsOnly("0", "0", "0");
     }
+
 }
