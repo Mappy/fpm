0b25aaa9ee59727b8152ac3c6f0bd3bbad50af0e
Ignishky <thierry.ducloyer@gmail.com>
2017-10-24 17:46:56 +0200

    [timeDomains] Parse starting month and duration.

--- src/main/java/com/mappy/fpm/batches/tomtom/dbf/timedomains/TimeDomainsParser.java
+++ src/main/java/com/mappy/fpm/batches/tomtom/dbf/timedomains/TimeDomainsParser.java
@@ -1,5 +1,7 @@
 package com.mappy.fpm.batches.tomtom.dbf.timedomains;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -7,26 +9,39 @@ import java.util.regex.Pattern;
 import static java.lang.Integer.valueOf;
 import static java.util.stream.Collectors.joining;
 
+@Slf4j
 public class TimeDomainsParser {
 
-    private static final Pattern PATTERN = Pattern.compile("\\[\\(h(\\d{1,2})\\)\\{h(\\d{1,2})\\}\\]"); // [(h11){h7}]
+    private static final Pattern HOUR_PATTERN = Pattern.compile("\\[\\(h(\\d{1,2})\\)\\{h(\\d{1,2})\\}\\]"); // [(h11){h7}]
+    private static final Pattern MONTH_PATTERN = Pattern.compile("\\[\\(M(\\d{1,2})\\)\\{M(\\d{1,2})\\}\\]"); // [(M3){M5}]
+
+    private enum Month {
+        Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec
+    }
 
     public String parse(Collection<TimeDomains> tomtomTimesDomains) {
 
         return tomtomTimesDomains.stream().map(this::parse).collect(joining(", "));
     }
 
-    private String parse(TimeDomains next) {
-        String domain = next.getDomain();
+    private String parse(TimeDomains timeDomains) {
 
-        String begin_hour = null;
-        String duration_hour = null;
-        Matcher matcher = PATTERN.matcher(domain);
-        if (matcher.find()) {
-            begin_hour = matcher.group(1);
-            duration_hour = matcher.group(2);
+        Matcher hourMatcher = HOUR_PATTERN.matcher(timeDomains.getDomain());
+        Matcher monthMatcher = MONTH_PATTERN.matcher(timeDomains.getDomain());
+
+        if (hourMatcher.find()) {
+            String begin_hour = hourMatcher.group(1);
+            String duration_hour = hourMatcher.group(2);
+            return String.format("%02d:00-%02d:00 off", valueOf(begin_hour), (valueOf(begin_hour) + valueOf(duration_hour)) % 24);
+
+        } else if (monthMatcher.find()) {
+            String begin_month = monthMatcher.group(1);
+            String duration_month = monthMatcher.group(2);
+            return String.format("%s-%s off", Month.values()[valueOf(begin_month) - 1], Month.values()[(valueOf(begin_month) + valueOf(duration_month) - 2) % 12]);
+
+        } else {
+            log.warn("Unable to parse '{}'", timeDomains.getDomain());
+            return "";
         }
-
-        return String.format("%02d:00-%02d:00 off", valueOf(begin_hour), (valueOf(begin_hour) + valueOf(duration_hour)) % 24);
     }
 }
--- src/test/java/com/mappy/fpm/batches/tomtom/dbf/timedomains/TimeDomainsParserTest.java
+++ src/test/java/com/mappy/fpm/batches/tomtom/dbf/timedomains/TimeDomainsParserTest.java
@@ -10,7 +10,15 @@ public class TimeDomainsParserTest {
     private final TimeDomainsParser parser = new TimeDomainsParser();
 
     @Test
-    public void should_translate_tomtom_time_domain_to_osm_openinghours(){
+    public void should_return_empty_string_when_time_domain_could_not_be_parsed() {
+
+        String openingHours = parser.parse(newHashSet(new TimeDomains(144, "non-parsable")));
+
+        assertThat(openingHours).isEqualTo("");
+    }
+
+    @Test
+    public void should_translate_tomtom_time_domain_to_osm_opening_hours(){
         TimeDomains tomtomTimesDomains = new TimeDomains(14420000000590L, "[(h6){h2}]");
         String osmTimeDomain = parser.parse(newHashSet(tomtomTimesDomains));
 
@@ -18,7 +26,7 @@ public class TimeDomainsParserTest {
     }
 
     @Test
-    public void should_translate_tomtom_time_domain_over_midnight_to_osm_openinghours(){
+    public void should_translate_tomtom_time_domain_over_midnight_to_osm_opening_hours(){
         TimeDomains tomtomTimesDomains = new TimeDomains(14420000000590L, "[(h22){h8}]");
         String osmTimeDomain = parser.parse(newHashSet(tomtomTimesDomains));
 
@@ -26,7 +34,25 @@ public class TimeDomainsParserTest {
     }
 
     @Test
-    public void should_translate_tomtom_time_domain_collection_to_osm_openinghours(){
+    public void should_translate_tomtom_time_domain_with_month_to_osm_opening_hours(){
+        TimeDomains tomtomTimesDomains = new TimeDomains(14420000000590L, "[(M3){M5}]");
+
+        String osmTimeDomain = parser.parse(newHashSet(tomtomTimesDomains));
+
+        assertThat(osmTimeDomain).isEqualTo("Mar-Jul off");
+    }
+
+    @Test
+    public void should_translate_tomtom_time_domain_with_month_over_year_to_osm_opening_hours(){
+        TimeDomains tomtomTimesDomains = new TimeDomains(14420000000590L, "[(M10){M5}]");
+
+        String osmTimeDomain = parser.parse(newHashSet(tomtomTimesDomains));
+
+        assertThat(osmTimeDomain).isEqualTo("Oct-Feb off");
+    }
+
+    @Test
+    public void should_translate_tomtom_time_domain_collection_to_osm_opening_hours(){
         TimeDomains tomtomTimesDomains = new TimeDomains(14420000000590L, "[(h6){h2}]");
         TimeDomains tomtomTimesDomains2 = new TimeDomains(14420000000590L, "[(h22){h8}]");
 
