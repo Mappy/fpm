3cd62c7484ba83a443f790942560cbf5c177f9f8
Ignishky <thierry.ducloyer@gmail.com>
2017-10-26 12:23:48 +0200

    [fix] Multi begin parsing.

--- src/main/java/com/mappy/fpm/batches/tomtom/dbf/timedomains/TimeDomainsParser.java
+++ src/main/java/com/mappy/fpm/batches/tomtom/dbf/timedomains/TimeDomainsParser.java
@@ -43,13 +43,20 @@ public class TimeDomainsParser {
 
     private String getOpeningHoursFromDuration(Matcher matcher) {
 
-        Element begin = parse(matcher.group(1));
-        Element duration = parse(matcher.group(2));
+        Element begin;
+        Element duration;
+        try {
+            begin = parse(matcher.group(1));
+            duration = parse(matcher.group(2));
+        } catch (NumberFormatException mfe) {
+            log.warn("Unable to parse duration {}", matcher.group(0));
+            return "";
+        }
 
-        if("h".equals(begin.getMode()) && "h".equals(duration.getMode())) {
+        if ("h".equals(begin.getMode()) && "h".equals(duration.getMode())) {
             return String.format("%02d:00-%02d:00 off", begin.getIndex(), (begin.getIndex() + duration.getIndex()) % 24);
 
-        } else if("M".equals(begin.getMode()) && "M".equals(duration.getMode())) {
+        } else if ("M".equals(begin.getMode()) && "M".equals(duration.getMode())) {
             return String.format("%s-%s off", Month.values()[begin.getIndex() - 1], Month.values()[(begin.getIndex() + duration.getIndex() - 2) % 12]);
         }
 
@@ -59,10 +66,17 @@ public class TimeDomainsParser {
 
     private String getOpeningHoursFromInterval(Matcher matcher) {
 
-        Element begin = parse(matcher.group(1));
-        Element end = parse(matcher.group(2));
+        Element begin;
+        Element end;
+        try {
+            begin = parse(matcher.group(1));
+            end = parse(matcher.group(2));
+        } catch (NumberFormatException mfe) {
+            log.warn("Unable to parse interval {}", matcher.group(0));
+            return "";
+        }
 
-        if("M".equals(begin.getMode()) && "M".equals(end.getMode())) {
+        if ("M".equals(begin.getMode()) && "M".equals(end.getMode())) {
             return String.format("%s-%s off", Month.values()[begin.getIndex() - 1], Month.values()[end.getIndex() - 1]);
         }
 
