bebea3e7b606751496114145226c673b5e312e2c
Thierry Ducloyer <thierry.ducloyer@mappy.com>
2017-12-11 17:31:11 +0100

    [Refactoring] Clean Frc & Fow.

--- src/main/java/com/mappy/fpm/batches/tomtom/helpers/Fow.java
+++ src/main/java/com/mappy/fpm/batches/tomtom/helpers/Fow.java
@@ -2,14 +2,43 @@ package com.mappy.fpm.batches.tomtom.helpers;
 
 import lombok.Getter;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static com.google.common.collect.ImmutableMap.of;
+
+@Getter
 public enum Fow {
-    PART_OF_MOTORWAY(1), PARKING_PLACE(6), PARKING_GARAGE_BUILDING(7), SLIP_ROAD(10), ENTRANCE_EXIT_CAR_PARK(12), ROUNDABOUT(4), PEDESTRIAN(14), WALKWAY(15), STAIRS(19);
+    PART_OF_MOTORWAY(1, of("foot", "no", "bicycle", "no")),
+    PART_OF_MULTI_CARRIAGEWAY(2, of()),
+    PART_OF_SINGLE_CARRIAGEWAY(3, of()),
+    ROUNDABOUT(4, of()),
+    PARKING_PLACE(6, of("highway", "service", "service", "parking_aisle")),
+    PARKING_GARAGE_BUILDING(7, of()),
+    UNSTRUCTURED_TRAFFIC_SQUARE(8, of()),
+    SLIP_ROAD(10, of()),
+    SERVICE_ROAD(11, of()),
+    ENTRANCE_EXIT_CAR_PARK(12, of("highway", "service", "service", "parking_aisle")),
+    PEDESTRIAN(14, of("highway", "pedestrian")),
+    WALKWAY(15, of("highway", "footway")),
+    SPECIAL_TRAFFIC_FIGURE(17, of()),
+    GALLERY(18, of()),
+    STAIRS(19, of("highway", "steps")),
+    ROAD_OF_AUTHORITIES(19, of()),
+    CONNECTOR(20, of()),
+    CUL_DE_SAC(22, of());
 
-    @Getter
     private final int value;
+    private final Map<String, String> tags;
 
-    Fow(int value) {
+    Fow(int value, Map<String, String> tags) {
         this.value = value;
+        this.tags = tags;
+    }
+
+    public static Optional<Fow> getFow(Integer value) {
+        return Stream.of(values()).filter(f -> f.value == value).findFirst();
     }
 
     public boolean is(Integer input) {
