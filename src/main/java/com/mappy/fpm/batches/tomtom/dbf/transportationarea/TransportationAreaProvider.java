8a7b68afe19e68b773f67a1d9823cd6fc5ecc69b
rbelhadef <rabah.belhadef.ext@mappy.com>
2018-03-28 15:35:48 +0200

    split bua:tomtom and admin:tomtom on bua:tomtom:left/bua:tomtom:right and admin:tomtom:left/admin:tomtom:right

--- src/main/java/com/mappy/fpm/batches/tomtom/dbf/transportationarea/TransportationAreaProvider.java
+++ src/main/java/com/mappy/fpm/batches/tomtom/dbf/transportationarea/TransportationAreaProvider.java
@@ -3,6 +3,7 @@ package com.mappy.fpm.batches.tomtom.dbf.transportationarea;
 import com.google.common.collect.ImmutableList;
 import com.mappy.fpm.batches.tomtom.TomtomFolder;
 import com.mappy.fpm.batches.tomtom.dbf.TomtomDbfReader;
+import com.mappy.fpm.batches.tomtom.dbf.transportationarea.TransportationArea.SideOfLine;
 import lombok.extern.slf4j.Slf4j;
 import org.jamel.dbf.structure.DbfRow;
 
@@ -16,11 +17,13 @@ import java.util.function.Predicate;
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Maps.newHashMap;
 import static com.mappy.fpm.batches.tomtom.dbf.transportationarea.TransportationArea.AreaType.isTheMinimumAreaType;
+import static com.mappy.fpm.batches.tomtom.dbf.transportationarea.TransportationArea.SideOfLine.LEFT;
+import static com.mappy.fpm.batches.tomtom.dbf.transportationarea.TransportationArea.SideOfLine.RIGHT;
 import static com.mappy.fpm.batches.tomtom.dbf.transportationarea.TransportationArea.TransportationElementType.isARoadElement;
-import static java.util.Comparator.comparing;
+import static java.util.Collections.emptyList;
+import static java.util.Comparator.naturalOrder;
 import static java.util.Optional.empty;
 import static java.util.Optional.ofNullable;
-import static java.util.stream.Collectors.joining;
 
 @Slf4j
 @Singleton
@@ -33,35 +36,39 @@ public class TransportationAreaProvider extends TomtomDbfReader {
         readFile("ta.dbf", this::getTransportationAreas);
     }
 
-    public Optional<String> getBuiltUp(Long tomtomId) {
-        return getTransportationAreas(tomtomId, getTransportationAreaPredicate(true));
+    public Optional<String> getBuiltUpLeft(Long tomtomId) {
+        return getTransportationAreas(tomtomId, getTransportationAreaPredicate(true), LEFT );
     }
 
-    public Optional<String> getSmallestAreas(Long tomtomId) {
-        Optional<TransportationArea> min = getMaxAreaType(tomtomId);
-
-        if (min.isPresent()) {
-            return getTransportationAreas(tomtomId, transportationArea -> transportationArea.getAreaType().equals(min.get().getAreaType()));
-        }
-        return empty();
+    public Optional<String> getBuiltUpRight(Long tomtomId) {
+        return getTransportationAreas(tomtomId, getTransportationAreaPredicate(true), RIGHT);
     }
 
-    private Optional<String> getTransportationAreas(Long tomtomId, Predicate<TransportationArea> transportationAreaPredicate) {
-        return ofNullable(ofNullable(transportationAreas.get(tomtomId))
-                .orElse(ImmutableList.of())
+    public Optional<String> getLeftSmallestAreas(Long tomtomId) {
+        return getMaxAreaType(tomtomId).map(max -> getTransportationAreas(tomtomId, tr -> max.equals(tr.getAreaType()), LEFT)).orElse(empty());
+    }
+
+
+    public Optional<String> getRightSmallestAreas(Long tomtomId) {
+        return getMaxAreaType(tomtomId).map(max -> getTransportationAreas(tomtomId, tr -> max.equals(tr.getAreaType()), RIGHT)).orElse(empty());
+    }
+
+    private Optional<String> getTransportationAreas(Long tomtomId, Predicate<TransportationArea> transportationAreaPredicate, SideOfLine sideOfLine) {
+        return transportationAreas.getOrDefault(tomtomId, emptyList())
                 .stream()
+                .filter(area -> sideOfLine.equals(area.getSideOfLine()))
                 .filter(transportationAreaPredicate)
-                .sorted(comparing(TransportationArea::getSideOfLine))
                 .map(t -> t.getAreaId().toString())
-                .collect(joining(";")));
+                .findFirst() ;
     }
 
-    private Optional<TransportationArea> getMaxAreaType(Long tomtomId) {
+    private Optional<Integer> getMaxAreaType(Long tomtomId) {
         return ofNullable(transportationAreas.get(tomtomId))
                 .orElse(ImmutableList.of())
                 .stream()
                 .filter(getTransportationAreaPredicate(false))
-                .max(comparing(TransportationArea::getAreaType));
+                .map(TransportationArea::getAreaType)
+                .max(naturalOrder());
     }
 
     private Predicate<TransportationArea> getTransportationAreaPredicate(Boolean needBuiltUp) {
@@ -74,5 +81,4 @@ public class TransportationAreaProvider extends TomtomDbfReader {
         transportationAreaList.add(geocode);
         transportationAreas.put(geocode.getId(), transportationAreaList);
     }
-
 }
