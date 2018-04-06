37620956a0f8c000f1f8dee173893f7b87af15b4
rbelhadef <rabah.belhadef.ext@mappy.com>
2018-04-06 15:45:33 +0200

    rename French-Guiana

--- src/main/java/com/mappy/fpm/batches/tomtom/download/TomtomCountries.java
+++ src/main/java/com/mappy/fpm/batches/tomtom/download/TomtomCountries.java
@@ -66,7 +66,7 @@ public class TomtomCountries {
         countries.add(new TomtomCountry("TUR", "Turkey"));
         countries.add(new TomtomCountry("UKR", "Ukraine"));
         countries.add(new TomtomCountry("GLP", "French-West-Indies"));
-        countries.add(new TomtomCountry("GUF", "French-Guyana"));
+        countries.add(new TomtomCountry("GUF", "French-Guiana"));
         countries.add(new TomtomCountry("REU", "Reunion-and-Mayotte"));
         return countries;
     }
