382eb55b9e665317117b288097bc4a154eb3c927
Gilles <bboy.stats@gmail.com>
2018-04-30 15:03:58 +0200

    [test] test loading alternate city name

--- src/test/java/com/mappy/fpm/batches/tomtom/dbf/names/NameProviderTest.java
+++ src/test/java/com/mappy/fpm/batches/tomtom/dbf/names/NameProviderTest.java
@@ -19,6 +19,7 @@ public class NameProviderTest {
     public void setUp() {
         when(tomtomFolder.getFile("an.dbf")).thenReturn("src/test/resources/tomtom/name/andorra___________an.dbf");
         when(tomtomFolder.getFile("lxnm.dbf")).thenReturn("src/test/resources/tomtom/name/andorra___________lxnm.dbf");
+        when(tomtomFolder.getFile("smnm.dbf")).thenReturn("src/test/resources/tomtom/name/andorra___________smnm.dbf");
         nameProvider = new NameProvider(tomtomFolder);
     }
 
@@ -26,6 +27,20 @@ public class NameProviderTest {
     public void should_add_alternative_names() {
         nameProvider.loadAlternateNames("an.dbf");
         Map<String, String> tags = nameProvider.getAlternateNames(10200000000008L);
+        assertThat(tags).hasSize(7);
+        assertThat(tags.get("name:ca")).isEqualTo("Andorra_cat");
+        assertThat(tags.get("name:fr")).isEqualTo("Andorre");
+        assertThat(tags.get("alt_name:fr")).isEqualTo("Principauté d'andorre");
+        assertThat(tags.get("name:de")).isEqualTo("Andorra_ger");
+        assertThat(tags.get("name:en")).isEqualTo("Andorra_eng");
+        assertThat(tags.get("name:es")).isEqualTo("Andorra_spa");
+        assertThat(tags.get("int_name")).isEqualTo("Andorra_aaa");
+    }
+
+    @Test
+    public void should_add_alternative_city_names() {
+        nameProvider.loadAlternateCityNames("smnm.dbf");
+        Map<String, String> tags = nameProvider.getAlternateCityNames(10200000000008L);
         assertThat(tags).hasSize(7);
         assertThat(tags.get("name:ca")).isEqualTo("Andorra_cat");
         assertThat(tags.get("name:fr")).isEqualTo("Andorre");
